addb06044e4e46725ede1519b8c7a52bb1d27879
Jan Andersson <jan.andersson@gmail.com>
2020-11-15 23:58:34 +0100

    Don't repair strong walls

--- src/utils/getTask.ts
+++ src/utils/getTask.ts
@@ -62,7 +62,8 @@ export const getTask = (creep: Creep): Task | null => {
     .find(FIND_STRUCTURES, {
       filter: structure =>
         (structure.structureType === STRUCTURE_WALL || structure.structureType === STRUCTURE_RAMPART) &&
-        structure.hits < structure.hitsMax
+        structure.hits < structure.hitsMax &&
+        structure.hits < 1_000_000
     })
     .sort((a, b) => a.hits - b.hits)[0];
   if (wallTarget) {
